989dd6e3c0565fe501026f6e8480688c91cca423
tariel <tarieltitirashvili@gmail.com>
2023-11-19 10:39:10 -0800

    initial app

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+require("dotenv").config();
+
+app = express()
+
+const port = process.env.PORT || 3000;
+
+const start = async () => {
+  try {
+    app.listen(port, () =>
+      console.log(`Server is listening on port ${port}...`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
